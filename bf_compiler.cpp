bd8cab8345e685ff05aab8166ac23cd27111c5a0
g.katsuki <t201d021@gunma-u.ac.jp>
2021-12-09 15:30:53 +0900

    first complete

--- bf_compiler.cpp
+++ bf_compiler.cpp
@@ -1,51 +1,84 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <new>
+#include <map>
+#include <vector>
 
 using namespace std;
 
-void identifySymbols(const string &line, int *ptr)
+void getSymbols(const string &line, map<int, char> &symbols, int &idx, int &comma)
 {
-  int idx = 0;
+  int count = 0;
   for(int i = 0, size = line.size(); i < size; i++){
-    if(line[i] == '+'){
-      ptr[idx]++;
+    if(line[i] != '+'){
+      symbols[i+idx] = line[i];
+    }
+    if(line[i] == '.') comma++;
+    count++;
+  }
+  idx += count;
+  // std::cout << line << std::endl;
+}
+
+void calcSymbols(const map<int, char> &symbols, vector<int> &output)
+{
+  int roop_count = 0, count_idx = 0, roop_idx = 0, past_symbol_idx = 0, output_count = 0;
+  vector<int> counts(output.size());
+  auto roop_it = symbols.begin();
+  for(auto it = symbols.begin(); it != symbols.end(); it++){
+    // cout << "first: " << it->first << endl; 
+    counts[count_idx] += it->first - past_symbol_idx - 1;
+    past_symbol_idx = it->first;
+    if(it->second == '>'){
+      count_idx++;
       continue;
     }
-    else if(line[i] == '>'){
-      idx++;
+    else if(it->second == '<'){
+      count_idx--;
       continue;
+    }
+    else if(it->second == '-'){
+      counts[count_idx]--;
+      // cout << "roop: " << counts[count_idx] << endl;
+      continue;
+    }
+    else if(it->second == '['){  // 2重ループ未対応
+      roop_idx = count_idx;
+      roop_it  = it;
+      continue;
+    }
+    else if(it->second == ']'){
+      if(counts[roop_idx] != 0){
+        it = roop_it;
+        past_symbol_idx = it->first;
+      }
+      continue;
+    }
+    else if(it->second == '.'){
+      output[output_count] = counts[count_idx];
+      output_count++;
     }
   }
-  std::cout << line << std::endl;
 }
 
 int main(int argc, char *argv[]){
   std::ifstream file;
   std::string filename = "./../sample/examples/hello.bf";
   file.open(filename, std::ios::in);
-  int count = 0, arr_size = 0;
   int *ptr;
-  int idx = 0;
+  int idx = 1, comma = 0;
+  map<int, char> symbols;
+  vector<int> output;
   std::string line;
   while(std::getline(file, line)){
-    for(int i = 0, size = line.size(); i < size; i++){
-      if(line[i] == '>') arr_size++;
-    }
-    std::cout << line << std::endl;
+    getSymbols(line, symbols, idx, comma);
   }
-
-  ptr = new int[arr_size];
-  file.close();
-  line.clear();
-  file.open(filename);
-  while(std::getline(file, line)){
-    identifySymbols(line, ptr);
+  output.resize(comma);
+  calcSymbols(symbols, output);
+  for(int i = 0, size = output.size(); i < size; i++){
+    // cout << "output: " << output[i] << endl;
+    printf("%c", output[i]);
   }
-
-  std::cout << "a" << ptr[0] << std::endl;
-  std::cout << "a" << ptr[1] << std::endl;
 
   return 0;
 }
