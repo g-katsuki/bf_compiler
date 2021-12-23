216ccda4d756eb749670acca52e243a76c613fd6
g.katsuki <t201d021@gunma-u.ac.jp>
2021-12-23 21:45:37 +0900

    isogi

--- bf_compiler.cpp
+++ bf_compiler.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <map>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
@@ -11,26 +12,29 @@ void getSymbols(const string &line, map<int, char> &symbols, int &idx, int &comm
   int count = 0;
   for(int i = 0, size = line.size(); i < size; i++){
     if(line[i] != '+'){
-      symbols[i+idx] = line[i];
+      symbols[i+idx] = line[i];  // スペースに対応できてない
     }
     if(line[i] == '.') comma++;
     count++;
   }
   idx += count;
-  // std::cout << line << std::endl;
+  std::cout << line << std::endl;
 }
 
 void calcSymbols(const map<int, char> &symbols, vector<int> &output)
 {
-  int roop_count = 0, count_idx = 0, roop_idx = 0, past_symbol_idx = 0, output_count = 0;
-  vector<int> counts(output.size());
+  int roop_count = 0, count_idx = 0, past_symbol_idx = 0, output_count = 0, size_count = 1;
+  vector<int> counts(1);
+  stack<int> roop_idx_stack;
+  stack<int> roop_place_stack;
   auto roop_it = symbols.begin();
   for(auto it = symbols.begin(); it != symbols.end(); it++){
-    // cout << "first: " << it->first << endl; 
+    // cout << "first: " << it->second << endl; 
     counts[count_idx] += it->first - past_symbol_idx - 1;
     past_symbol_idx = it->first;
     if(it->second == '>'){
       count_idx++;
+      if(size_count < count_idx + 1) counts.push_back(0);
       continue;
     }
     else if(it->second == '<'){
@@ -42,28 +46,43 @@ void calcSymbols(const map<int, char> &symbols, vector<int> &output)
       // cout << "roop: " << counts[count_idx] << endl;
       continue;
     }
-    else if(it->second == '['){  // 2重ループ未対応
-      roop_idx = count_idx;
-      roop_it  = it;
+    else if(it->second == '['){
+      roop_idx_stack.push(count_idx);
+      roop_place_stack.push(it->first);
+      cout << "QQ: " << roop_place_stack.top() << endl;
       continue;
     }
     else if(it->second == ']'){
-      if(counts[roop_idx] != 0){
-        it = roop_it;
+      //cout << "sta" << counts[roop_idx_stack.top()] << endl;
+      if(counts[roop_idx_stack.top()] != 0){
+        it = symbols.find(roop_place_stack.top());  // ここから！！！
+        //cout << "roop: " << roop_place_stack.top() << endl;
         past_symbol_idx = it->first;
+      } else {
+        //cout << "A" << endl;
+        roop_idx_stack.pop();
+        roop_place_stack.pop();
+        //cout << "S" << endl;
       }
+    // cout << "F: " << roop_idx_stack.size() << endl;
       continue;
     }
     else if(it->second == '.'){
-      output[output_count] = counts[count_idx];
-      output_count++;
+        // cout << "D" << endl;
+        // cout << "num: " << counts[count_idx] << endl;
+        // cout << "num0: " << counts[0] << endl;
+        // cout << "num1: " << counts[1] << endl;
+        // cout << "num2: " << counts[2] << endl;
+        // cout << "idx: " << count_idx << endl;
+      printf("%c", counts[count_idx]);
     }
   }
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
   std::ifstream file;
-  std::string filename = "./../sample/examples/hello.bf";
+  std::string filename = "./../sample/examples/test.bf";
   file.open(filename, std::ios::in);
   int *ptr;
   int idx = 1, comma = 0;
@@ -75,10 +94,10 @@ int main(int argc, char *argv[]){
   }
   output.resize(comma);
   calcSymbols(symbols, output);
-  for(int i = 0, size = output.size(); i < size; i++){
-    // cout << "output: " << output[i] << endl;
-    printf("%c", output[i]);
-  }
+//   for(int i = 0, size = output.size(); i < size; i++){
+//     //cout << "output: " << output[i] << endl;
+//     //printf("%c", output[i]);
+//   }
 
   return 0;
 }
