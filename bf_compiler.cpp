66165ffabf7e76fbed03b794aa4a24bb4ee972b8
g.katsuki <t201d021@gunma-u.ac.jp>
2022-01-11 14:46:02 +0900

    edit message

--- bf_compiler.cpp
+++ bf_compiler.cpp
@@ -8,8 +8,12 @@
 using namespace std;
 using std::cout;
 
+/*****************************************************************
+* 与えられた行に対して, map型にインデックスと記号を格納する関数
+******************************************************************/
 void getSymbols(const string &line, map<int, char> &symbols, int &idx, int &comma, int &space)
 {
+  /*** '+'以外の記号を格納してその間を'+'と考える. スペースはカウントしておいて格納時に引く ***/
   int count = 0;
   for(int i = 0, size = line.size(); i < size; i++){
     if(line[i] == ' ') space++;
@@ -20,12 +24,15 @@ void getSymbols(const string &line, map<int, char> &symbols, int &idx, int &comm
     count++;
   }
   idx += count;
-  std::cout << line << std::endl;
+  // std::cout << line << std::endl;
 }
 
-void calcSymbols(const map<int, char> &symbols, vector<int> &output)
+/****************************************************************
+* mapのインデックスと記号に対しての操作を行う関数
+*****************************************************************/
+void calcSymbols(const map<int, char> &symbols)
 {
-  int roop_count = 0, count_idx = 0, past_symbol_idx = 0, output_count = 0, size_count = 1;
+  int roop_count = 0, count_idx = 0, past_symbol_idx = 0, size_count = 1;
   vector<int> counts(1);
   stack<int> roop_idx_stack;
   stack<int> roop_place_stack;
@@ -37,36 +44,40 @@ void calcSymbols(const map<int, char> &symbols, vector<int> &output)
     counts[count_idx] += it->first - past_symbol_idx - 1;
     past_symbol_idx = it->first;
     if(it->second == '>'){
+      /*** カウントするインデックスを+1 ***/
       count_idx++;
       if(size_count < count_idx + 1) counts.push_back(0);
       continue;
     }
     else if(it->second == '<'){
+      /*** カウントするインデックスを-1 ***/
       count_idx--;
       continue;
     }
     else if(it->second == '-'){
+      /*** 現在のカウントを-1 ***/
       counts[count_idx]--;
       continue;
     }
     else if(it->second == '['){
+      /*** 現在の場所をスタックにpush ***/
       roop_place_stack.push(it->first);
       continue;
     }
     else if(it->second == ']'){
       if(counts[count_idx] > 0){
+        /*** 現在のカウントが0より大きければスタックの位置に戻る ***/
         it = symbols.find(roop_place_stack.top());
         past_symbol_idx = it->first;
       } else {
+        /*** 現在のカウントが0以下ならスタックをpop ***/
         roop_place_stack.pop();
       }
       continue;
     }
     else if(it->second == '.'){
-      dot_count_for_debug++;
+      /*** 出力 ***/
       printf("%c", counts[count_idx]);
-      output[output_count] = counts[count_idx];
-      output_count++;
     }
   }
 }
@@ -80,15 +91,13 @@ int main(int argc, char **argv)
   int *ptr;
   int idx = 1, comma = 0, space = 0;
   map<int, char> symbols;
-  vector<int> output;
   std::string line;
+  
   while(std::getline(file, line)){
     getSymbols(line, symbols, idx, comma, space);
   }
-  output.resize(1000);
 
-  calcSymbols(symbols, output);
-  cout << "size: " << output.size() << endl;
+  calcSymbols(symbols);
 
   return 0;
 }
